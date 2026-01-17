a00373acbfd1a235880ff15dc701caefe2549063
ToniTrum <kulahszjan@gmail.com>
2026-01-18 00:23:05 +1000

    connected DRF

--- TechTaskBitrix/TechTaskBitrix/wsgi.py
+++ TechTaskBitrix/TechTaskBitrix/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TechTaskBitrics project.
+WSGI config for TechTaskBitrix project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TechTaskBitrics.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TechTaskBitrix.settings')
 
 application = get_wsgi_application()
